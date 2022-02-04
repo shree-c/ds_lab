fc82d6fdf9c6e0d0d4ce8e92db2b600c34fb2d90
shree <sammmmmu123@gmail.com>
2022-02-04 22:55:30 +0530

    minor changes

--- script.sh
+++ script.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-
-sudo apt install git
+wget -q --spider http://google.com
+if [ $? -ne 0 ]; then
+    echo "!!!! no internet"
+fi
+if [ -d ~/ds ]; then
+	rm -rf ~/ds
+fi
+if !command git &> /dev/null
+then
+	if command apt &> /dev/null
+	then
+		sudo apt install git
+	elif command pacman &> /dev/null
+	then
+		sudo pacman -S git
+	elif command pkg &> /dev/null
+	then
+		pkg install git
+	fi
+fi
 
 git clone https://github.com/shree-c/ds_lab ~/ds
 
